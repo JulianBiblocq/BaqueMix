0dc46ab1408a7f9788278121461ee293c4c8fcc4
julianbiblocq <julianbiblocq@users.noreply.github.com>
2026-06-08 10:14:02 +0200

    fix: bump service worker cache version to v3 and add update reload prompt in index.html

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const CACHE_NAME = 'baquemix-cache-v2';
+const CACHE_NAME = 'baquemix-cache-v3';
 
 // Core static files to cache immediately on SW install
 const STATIC_ASSETS = [
