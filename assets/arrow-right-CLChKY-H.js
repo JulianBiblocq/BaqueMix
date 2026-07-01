dfb0caa0b495cc6830b84b56332f317a7afec72b
julianbiblocq <julianbiblocq@users.noreply.github.com>
2026-07-01 20:41:59 +0200

    Updates

--- assets/arrow-right-CLChKY-H.js
+++ assets/arrow-right-CLChKY-H.js
@@ -1,4 +1,4 @@
-import{m as o}from"./index-SvwqgQnT.js";/**
+import{m as o}from"./index-CcsNKZEF.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
